c689e64235a5b45e9d5dc6589cb958dfeccf6f89
zhangir003 <zhangir003@mail.ru>
2024-11-17 13:40:15 +0500

    AddDtos

---
+++ NetShop/NetShop.Dto/Dtos/ProductsDto/GetProductDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetShop.Dto.Dtos.ProductsDto
+{
+    internal class GetProductDto
+    {
+    }
+}
---
+++ NetShop/NetShop.Dto/Dtos/ProductsDto/UpdateProductDto.cs
@@ -0,0 +1,14 @@
+﻿using NetShop.Dto.Dtos.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetShop.Dto.Dtos.ProductsDto
+{
+    public class UpdateProductDto:BaseProductDto, IUpdate
+    {
+        public DateTime UpdatedTime { get; set; }
+    }
+}
