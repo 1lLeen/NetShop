7c48d2d1cbedf0c81c9b7f7ec3aaf341acf1c2b8
zhangir003 <zhangir003@mail.ru>
2024-11-17 15:40:50 +0500

    AddModels

---
+++ NetShop/NetShop.Application/Models/Products/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetShop.Application.Models.Products
+{
+    public class ProductModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Description2 { get; set; }
+        public Guid CategoryId { get; set; }
+
+    }
+}
