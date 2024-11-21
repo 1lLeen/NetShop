169e38a8fa7245969333bd45b4a743b6b4a9f998
zhangir003 <zhangir003@mail.ru>
2024-11-21 17:04:36 +0500

    RefactoringOfRegistration

---
+++ NetShop/NetShop.Infrastucture/Registration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NetShop.Application.Repositories.Interfaces;
+using NetShop.Application.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetShop.Infrastucture.Servicese.Interfaces;
+using NetShop.Infrastucture.Servicese;
+
+namespace NetShop.Infrastucture.MappingConfig
+{
+    public static class Registration
+    {
+        public static void RegistrationRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<ICategoryRepository, CategoryRepoistory>();
+            services.AddScoped<IProductRepository, ProductRepoistory>();
+
+        }
+        public static void RegistrationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+        }
+    }
+}
--- NetShop/NetShop.UI/Program.cs
+++ NetShop/NetShop.UI/Program.cs
@@ -1,7 +1,22 @@
+using NetShop.Application;
+using Microsoft.EntityFrameworkCore;
+using NetShop.Infrastucture;
+using NetShop.Infrastucture.MappingConfig;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Configuration
+.AddJsonFile($"appsettings.json", optional: false)
+.AddJsonFile($"appsettings.Environment.json", optional: true)
+.AddEnvironmentVariables()
+.Build();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<NetShopDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaulConneection")));
+builder.Services.RegistrationRepositories();
+builder.Services.RegistrationServices();
+builder.Services.RegistrationAutoMapper();
 
 var app = builder.Build();
 
@@ -21,5 +36,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+app.MapControllers();
 
 app.Run();
