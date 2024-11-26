c7022ad4d6f49b8a43a4c5c9f5771f1649540dc2
zhangir003 <zhangir003@mail.ru>
2024-11-26 14:30:24 +0500

    AddUIController

---
+++ NetShop/NetShop.UI/Controllers/MainController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetShop.Application.Servicese.Interfaces;
+
+namespace NetShop.UI.Controllers
+{
+    public class MainController : Controller
+    {
+        protected readonly IProductService productService;
+        protected readonly ICategoryService categoryService;
+        private ILogger logger;
+        public MainController(IProductService productService, ICategoryService categoryService, ILogger logger)
+        {
+            this.productService = productService;
+            this.categoryService = categoryService;
+            this.logger = logger;
+        }
+        public ActionResult Index()
+        {
+            return View(productService.GetAll());
+        }
+
+    }
+}
